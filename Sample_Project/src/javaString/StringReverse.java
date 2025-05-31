df8f9e12d4be88d25095c758a7e778ab9c8d5c29
sugandev13 <suganthcal4care@gmail.com>
2025-05-31 12:28:30 +0530

    latest updated file.

--- Sample_Project/src/javaString/StringReverse.java
+++ Sample_Project/src/javaString/StringReverse.java
@@ -1,10 +1,13 @@
 package javaString;
 
+import java.util.Arrays;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// String Reverse
 		String g = "Uploading a program files to the GIT";
 		StringBuilder g1 = new StringBuilder(g);
 		g1.reverse();
@@ -16,6 +19,24 @@ public class StringReverse {
 			g2 += g.charAt(i);
 		}
 		System.out.println(g2);
+		
+		
+		// String Split
+		String [] g3 = g.split(" ");
+		for (String g4 : g3) {
+			System.out.println(g4);
+		}
+		System.out.println(" ");
+		// String Arrays
+		
+		String g5 [] = {"Selenium","Java","Eclipse","Github","Upload"};
+		System.out.println(g5[3]);
+		
+		// call all the arrays
+		System.out.println(Arrays.toString(g5));
+		
+		// call last one
+		System.out.println(g5[g5.length-1]);
 
 	}
 
