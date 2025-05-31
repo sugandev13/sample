b1d67c2f5ad3c539cd4aeedf8c61771fb45c631a
sugandev13 <suganthcal4care@gmail.com>
2025-05-31 12:14:17 +0530

    sample uploading files.

---
+++ Sample_Project/src/javaString/StringReverse.java
@@ -0,0 +1,16 @@
+package javaString;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String g = "Uploading a program files to the GIT";
+		StringBuilder g1 = new StringBuilder(g);
+		g1.reverse();
+		System.out.println(g1);
+		System.out.println(g);
+
+	}
+
+}
